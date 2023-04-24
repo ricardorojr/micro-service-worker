a656339887426125496fa026b437b1b96eda1066
Ricardo Rodrigues de Oliveira Júnior <ricardorodriguesjr87@gmail.com>
2023-04-24 12:32:56 -0300

    using rest-template for comunication bettein microservices

--- hr-payroll/src/main/java/com/ricardo/payroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/ricardo/payroll/services/PaymentService.java
@@ -1,13 +1,37 @@
 package com.ricardo.payroll.services;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.xml.bind.ParseConversionEvent;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.ricardo.payroll.entities.Payment;
+import com.ricardo.payroll.entities.Worker;
 
 @Service
 public class PaymentService {
 	
+	@Value("${hr-worker-host}")
+	private String WorkerHost;
+	
+	@Value("${hr-worker-path}")
+	private String WorkerPath;
+	
+	
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	
 	public Payment getPayment(long workerId, int days) {
-		return new Payment("Ricardo", 200.0, days);
+		Map<String, String> uriVariables = new HashMap<>();
+		uriVariables.put("id", ""+workerId);
+		
+		Worker worker = restTemplate.getForObject(WorkerHost + WorkerPath + "{id}", Worker.class, uriVariables);
+		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 }
