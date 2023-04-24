9787bea226d3a8df99e62ca603d526cb6b221997
Ricardo Rodrigues de Oliveira Júnior <ricardorodriguesjr87@gmail.com>
2023-04-24 18:14:47 -0300

    Config-server created

---
+++ hr-config-server/src/test/java/com/ricardo/hrconfigserver/HrConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ricardo.hrconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
