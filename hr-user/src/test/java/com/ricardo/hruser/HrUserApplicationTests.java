910c45c22aaf612d9282b9344ba64ab9d82d76b8
Ricardo Rodrigues de Oliveira Júnior <ricardorodriguesjr87@gmail.com>
2023-04-25 13:52:27 -0300

    Project hr-user created

---
+++ hr-user/src/test/java/com/ricardo/hruser/HrUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ricardo.hruser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
